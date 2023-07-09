450bb932302c7ad0cc08fdc9b7862e2bcb047e90
Justin Varela <71732039+justin-rgb@users.noreply.github.com>
2023-07-09 16:46:26 -0600

    Docs Actualizado

---
+++ docs/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const socketio = require('socket.io')
+const http = require('http')
+const path = require('path')
+const dotenv = require('dotenv')
+dotenv.config()
+//Llamar al modulo CORS para que no lo bloquee el hosting
+const cors = require('cors')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+require('./socket')(io)
+
+//SE CAMBIA EL PUERTO PARA QUE NO LO BLOQUEE EL HOSTING
+app.set('port', process.env.PORT_DEV || 5000)
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors())
+
+
+
+server.listen( app.get('port'), ()=>{
+    console.log(`La app esta corriendo en el puerto ${app.get('port')}`)
+})
